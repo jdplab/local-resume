b3a13d48e0b18f08013715440766810dd0b0b50b
jplab <jon@jon-polansky.com>
2024-02-14 15:09:18 -0500

    Testing Terraform Plan with Proxmox

--- main.tf
+++ main.tf
@@ -6,3 +6,46 @@ terraform {
     }
   }
 }
+provider "proxmox" {
+  pm_api_url = "https://192.168.69.251:8006/api2/json"
+  # api token id is in the form of: <username>@pam!<tokenId>
+  pm_api_token_id = "terraform-prov@pve!terraformToken"
+  # this is the full secret wrapped in quotes. don't worry, I've already deleted this from my proxmox cluster by the time you read this post
+  pm_api_token_secret = "a7b5c294-5bbf-4736-aa6a-ce0d23616307"
+  # leave tls_insecure set to true unless you have your proxmox SSL certificate situation fully sorted out (if you do, you will know)
+  pm_tls_insecure = true
+}
+
+resource "proxmox_vm_qemu" "test_server" {
+  count = 1 
+  name = "test-vm-${count.index + 1}" 
+  target_node = var.proxmox_host
+  clone = var.template_name
+  agent = 1
+  os_type = "cloud-init"
+  cores = 2
+  sockets = 1
+  cpu = "host"
+  memory = 2048
+  scsihw = "virtio-scsi-pci"
+  bootdisk = "scsi0"
+  disk {
+    slot = 0
+    size = "10G"
+    type = "scsi"
+    storage = "jp-emnas01"
+    iothread = 1
+  }
+
+  network {
+    model = "virtio"
+    bridge = "vmbr0"
+  }
+  # not sure exactly what this is for. presumably something about MAC addresses and ignore network changes during the life of the VM
+  lifecycle {
+    ignore_changes = [
+      network,
+    ]
+  }
+  ipconfig0 = "ip=192.168.69.6${count.index + 1}/24,gw=192.168.69.1"
+}
