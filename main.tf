8f6ce9998c5e7fdfd6d2e4b4511e22676fdd04f8
jplab <jon@jon-polansky.com>
2024-02-15 09:58:19 -0500

    Added webserver

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ provider "proxmox" {
 }
 
 resource "proxmox_vm_qemu" "db_server" {
-  count = 0 
+  count = 1 
   name = var.dbserver_name
   vmid = var.db_vmid
   target_node = var.proxmox_host
@@ -52,4 +52,44 @@ resource "proxmox_vm_qemu" "db_server" {
   }
 
   ipconfig0 = "ip=192.168.69.61/24,gw=192.168.69.1"
+}
+
+resource "proxmox_vm_qemu" "web_server" {
+  count = 1 
+  name = var.webserver_name
+  vmid = var.web_vmid
+  target_node = var.proxmox_host
+  clone = var.template_name
+  cloudinit_cdrom_storage = var.storage_location
+  agent = 1
+  os_type = "cloud-init"
+  cores = 2
+  sockets = 1
+  cpu = "host"
+  memory = 2048
+  scsihw = "virtio-scsi-pci"
+  bootdisk = "scsi0"
+  boot = "order=scsi0;ide3"
+  disks {
+    scsi {
+      scsi0 {
+        disk {
+          size = 10
+          storage = var.storage_location
+        }
+      }
+    }
+  }
+  network {
+    model = "virtio"
+    bridge = "vmbr0"
+  }
+
+  lifecycle {
+    ignore_changes = [
+      network,
+    ]
+  }
+
+  ipconfig0 = "ip=192.168.69.62/24,gw=192.168.69.1"
 }
