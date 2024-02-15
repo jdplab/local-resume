64d2f44cf478446f5e863d6cf2a17c01f10596b7
jplab <jon@jon-polansky.com>
2024-02-15 09:32:40 -0500

    Testing suggestions at: https://github.com/Telmate/terraform-provider-proxmox/issues/901#issuecomment-1939589804

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
     proxmox = {
-      source = "telmate/proxmox"
-      version = "3.0.1-rc1"
+      source = "bgp/proxmox"
+      version = "0.46.3"
     }
   }
 }
@@ -20,6 +20,7 @@ resource "proxmox_vm_qemu" "db_server" {
   vmid = var.db_vmid
   target_node = var.proxmox_host
   clone = var.template_name
+  cloudinit_cdrom_storage = var.storage_location
   agent = 1
   os_type = "cloud-init"
   cores = 2
@@ -28,14 +29,17 @@ resource "proxmox_vm_qemu" "db_server" {
   memory = 2048
   scsihw = "virtio-scsi-pci"
   bootdisk = "scsi0"
-  disk {
-    size = "10G"
-    type = "scsi"
-    storage = var.storage_location
-    iothread = 1
-    ssd = 1
+  boot = "order=scsi0;ide3"
+  disks {
+    scsi {
+      scsi0 {
+        disk {
+          size = "10G"
+          storage = var.storage_location
+        }
+      }
+    }
   }
-
   network {
     model = "virtio"
     bridge = "vmbr0"
@@ -48,40 +52,4 @@ resource "proxmox_vm_qemu" "db_server" {
   }
 
   ipconfig0 = "ip=192.168.69.61/24,gw=192.168.69.1"
-}
-
-resource "proxmox_vm_qemu" "web_server" {
-  count = 1 
-  name = var.webserver_name
-  vmid = var.web_vmid 
-  target_node = var.proxmox_host
-  clone = var.template_name
-  agent = 1
-  os_type = "cloud-init"
-  cores = 2
-  sockets = 1
-  cpu = "host"
-  memory = 2048
-  scsihw = "virtio-scsi-pci"
-  bootdisk = "scsi0"
-  disk {
-    size = "10G"
-    type = "scsi"
-    storage = var.storage_location
-    iothread = 1
-    ssd = 1
-  }
-
-  network {
-    model = "virtio"
-    bridge = "vmbr0"
-  }
-
-  lifecycle {
-    ignore_changes = [
-      network,
-    ]
-  }
-
-  ipconfig0 = "ip=192.168.69.62/24,gw=192.168.69.1"
 }
