3926d83145e7af97996cd52e5a16025d9d5ca83f
jplab <jon@jon-polansky.com>
2024-02-16 12:46:25 -0500

    Set up flask app, installing dependencies, reorganized

---
+++ frontend/app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template
+import sqlite3
+
+app = Flask(__name__)
+
+def get_visitor_count():
+    try:
+        connection = sqlite3.connect("/mnt/nfs/visitors.db")
+        cursor = connection.cursor()
+        cursor.execute("SELECT count FROM visitors")
+        count = cursor.fetchone()[0]
+        cursor.execute("UPDATE visitors SET count = count + 1")
+        connection.commit()
+        return count
+    except Exception as e:
+        print("Error:", e)
+        count = 0
+        return count
+    finally:
+        if connection:
+            connection.close()
+
+@app.route("/")
+def index():
+    count = get_visitor_count()
+    return render_template("index.html", visitor_count=count)
+
+if __name__ == "__main__":
+    app.run(debug=True)  # Set debug=False for production
