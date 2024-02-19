ab3bc4dd308affee4ec855ae47e7cb8aeefb24cf
jplab <jon@jon-polansky.com>
2024-02-19 15:01:56 -0500

    Made major changes to python code in an attempt to record and display unique visitors

--- frontend/appdev.py
+++ frontend/appdev.py
@@ -15,8 +15,50 @@ def get_visitor_count():
         return count
     except Exception as e:
         print("Error:", e)
-        count = 0
         return count
+    finally:
+        if connection:
+            connection.close()
+
+def get_unique_visitors():
+    connection = None
+    try:
+        connection = sqlite3.connect("/mnt/nfs/visitors.db")
+        cursor = connection.cursor()
+        cursor.execute("CREATE TABLE IF NOT EXISTS uniquevisitors(id int PRIMARY KEY, ip_address text, visits int)")
+        ip_address = str(request.headers.get("HTTP_X_REAL_IP"))
+        cursor.execute("SELECT visits FROM visitors WHERE ip_address = ?", (ip_address,))
+        visits = int(cursor.fetchone()[0])
+        if len(visits) == 0:
+            cursor.execute("INSERT INTO uniquevisitors(ip_address, visits) VALUES (?, 1)", (ip_address,))
+            cursor.execute("SELECT COUNT(*) FROM uniquevisitors")
+            uniquevisitors = cursor.fetchone()[0]
+        else:
+            cursor.execute("UPDATE uniquevisitors SET visits = ? WHERE ip_address = ?", (visits + 1, ip_address,))
+            cursor.execute("SELECT COUNT(*) FROM uniquevisitors")
+            uniquevisitors = cursor.fetchone()[0]
+        connection.commit()
+        return uniquevisitors
+    except Exception as e:
+        print("Error:", e)
+        return uniquevisitors
+    finally:
+        if connection:
+            connection.close()
+
+def get_visit_count():
+    connection = None
+    try:
+        connection = sqlite3.connect("/mnt/nfs/visitors.db")
+        cursor = connection.cursor()
+        ip_address = str(request.headers.get("HTTP_X_REAL_IP"))
+        cursor.execute("SELECT visits FROM visitors WHERE ip_address = ?", (ip_address,))
+        visits = int(cursor.fetchone()[0])
+        connection.commit()
+        return visits
+    except Exception as e:
+        print("Error:", e)
+        return visits
     finally:
         if connection:
             connection.close()
@@ -24,7 +66,9 @@ def get_visitor_count():
 @app.route("/")
 def index():
     count = get_visitor_count()
-    return render_template("index.html", visitor_count=count)
+    uniquevisitors = get_unique_visitors()
+    visits = get_visit_count()
+    return render_template("index.html", visitor_count=count, unique_visitors=uniquevisitors, user_visits=visits)
 
 @app.route("/JonathanPolanskyResume.docx")
 def static_from_root():
