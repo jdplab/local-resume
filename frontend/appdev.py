2f232d0a51e5cb0edc0095f2fdff395a5b4a0a9b
jdplab <Jpolansky1996@gmail.com>
2024-02-20 21:07:33 -0500

    update appdev.py

--- frontend/appdev.py
+++ frontend/appdev.py
@@ -10,10 +10,6 @@ redis_url = os.environ.get("REDIS_URL", "redis://localhost:6379/0")
 redis_pool = ConnectionPool.from_url(redis_url)
 redis_client = Redis(connection_pool=redis_pool)
 
-ip_address = request.headers.get("X-Forwarded-For", request.remote_addr)
-if ip_address.startswith("10.") or ip_address.startswith("172.") or ip_address.startswith("192."):
-    ip_address = "home"
-
 def get_visitor_count():
     return int(redis_client.get('visitor_count') or 0)
 
@@ -25,6 +21,11 @@ def get_unique_visitors_count():
 
 @app.route("/")
 def index():
+    
+    ip_address = request.headers.get("X-Forwarded-For", request.remote_addr)
+    if ip_address.startswith("10.") or ip_address.startswith("172.") or ip_address.startswith("192."):
+        ip_address = "home"
+  
     count = get_visitor_count()
     uniquevisitors = get_unique_visitors_count()
     visits = get_user_visits(ip_address)
@@ -36,6 +37,11 @@ def static_from_root():
 
 @app.route('/update-stats')
 def update_stats():
+  
+    ip_address = request.headers.get("X-Forwarded-For", request.remote_addr)
+    if ip_address.startswith("10.") or ip_address.startswith("172.") or ip_address.startswith("192."):
+        ip_address = "home"
+  
     redis_client.incr('visitor_count')
     redis_client.hincrby('user_visits', ip_address, 1)
     stats = {"visitor_count": get_visitor_count(), "unique_visitors": get_unique_visitors_count(), "user_visits": get_user_visits(ip_address)}
