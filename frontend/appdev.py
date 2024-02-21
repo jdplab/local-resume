6567520dd8fa2acacaee86cc3b52ee4a1870cceb
jplab <jon@jon-polansky.com>
2024-02-21 14:03:41 -0500

    Added ip_address argument to event_stream()

--- frontend/appdev.py
+++ frontend/appdev.py
@@ -44,7 +44,7 @@ def static_from_root():
 @app.route('/stream')
 def stream():
     ip_address = session.get("ip_address")
-    def event_stream():
+    def event_stream(ip_address):
         initial_data = json.dumps({
             "visitor_count": get_visitor_count(),
             "unique_visitors": get_unique_visitors_count(),
