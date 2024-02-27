4cff05592874cff985eac7aacc326b0ff6ec4b86
jplab <jon@jon-polansky.com>
2024-02-27 08:59:41 -0500

    Configured exception handling and logging

--- frontend/app.py
+++ frontend/app.py
@@ -4,8 +4,11 @@ from gevent import monkey
 import json
 import atexit
 import time
+import logging
 
 monkey.patch_all()
+
+logging.basicConfig(level=logging.ERROR, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 app = Flask(__name__, static_folder="static")
 app.secret_key = "secret_key"
@@ -22,36 +25,48 @@ def get_unique_visitors_count():
 
 @app.route("/")
 def index():
-    ip_address = request.headers.get("X-Forwarded-For", request.remote_addr)
-    if ip_address.startswith("10.") or ip_address.startswith("172.") or ip_address.startswith("192."):
-        ip_address = "home"
-    session["ip_address"] = ip_address
-    redis_client.incr('visitor_count')
-    redis_client.hincrby('user_visits', ip_address, 1)
-    stats = {"visitor_count": get_visitor_count(), "unique_visitors": get_unique_visitors_count()}
-    return render_template('index.html', stats=stats)
+    try:
+        ip_address = request.headers.get("X-Forwarded-For", request.remote_addr)
+        if ip_address.startswith("10.") or ip_address.startswith("172.") or ip_address.startswith("192."):
+            ip_address = "home"
+        session["ip_address"] = ip_address
+        redis_client.incr('visitor_count')
+        redis_client.hincrby('user_visits', ip_address, 1)
+        stats = {"visitor_count": get_visitor_count(), "unique_visitors": get_unique_visitors_count()}
+        return render_template('index.html', stats=stats)
+    except Exception as e:
+        logging.error(f"Error in index route: {e}")
+        raise
 
 @app.route("/JonathanPolanskyResume.docx")
 def static_from_root():
-    return send_from_directory(app.static_folder, request.path[1:])
+    try:
+        return send_from_directory(app.static_folder, request.path[1:])
+    except Exception as e:
+        logging.error(f"Error downloading resume: {e}")
+        raise
 
 @app.route('/stream')
 def stream():
-    ip_address = session.get("ip_address")
-    
-    def event_stream(ip_address):
-        while True:
-            try:
-                data = {
-                    "visitor_count": get_visitor_count(),
-                    "unique_visitors": get_unique_visitors_count()
-                }
-                yield f'data: {json.dumps(data)}\n\n'
-                time.sleep(1) 
-            except GeneratorExit:
-                break
-    
-    return Response(event_stream(ip_address), mimetype='text/event-stream')
+    try:
+        ip_address = session.get("ip_address")
+        
+        def event_stream(ip_address):
+            while True:
+                try:
+                    data = {
+                        "visitor_count": get_visitor_count(),
+                        "unique_visitors": get_unique_visitors_count()
+                    }
+                    yield f'data: {json.dumps(data)}\n\n'
+                    time.sleep(1) 
+                except GeneratorExit:
+                    break
+        
+        return Response(event_stream(ip_address), mimetype='text/event-stream')
+    except Exception as e:
+        logging.error(f"Error in stream route: {e}")
+        raise
 
 def cleanup():
     redis_client.close()
