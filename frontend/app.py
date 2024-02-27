b6153b05d561971ac3b05c802b69420124bb4fe1
jplab <jon@jon-polansky.com>
2024-02-27 07:48:30 -0500

    Removed user_visits for performance

--- frontend/app.py
+++ frontend/app.py
@@ -1,14 +1,16 @@
-from flask import Flask, render_template, request, send_from_directory, Response, session
+from flask import Flask, render_template, request, send_from_directory, Response, session, jsonify
 from redis import Redis, ConnectionPool
+from gevent import monkey
 import logging
-import json
 import signal
 import sys
+import time
 
-logging.basicConfig(level=logging.DEBUG)
-logging.debug('logging is working')
+monkey.patch_all()
+
+logging.basicConfig(level=logging.WARNING)
 file_handler = logging.FileHandler('flask.log')
-file_handler.setLevel(logging.DEBUG)
+file_handler.setLevel(logging.WARNING)
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 file_handler.setFormatter(formatter)
 
@@ -17,25 +19,12 @@ app.secret_key = "secret_key"
 app.config['REDIS_URL'] = "redis://localhost:6379/0"
 app.logger.addHandler(file_handler)
 
-# Initialize Redis connection pool
-redis_pool = ConnectionPool.from_url(app.config['REDIS_URL'])
-redis_client = Redis(connection_pool=redis_pool)
-
-# Initialize Redis pub/sub connection
-pubsub = redis_client.pubsub()
-pubsub.subscribe('stats_channel')
-
-@app.teardown_request
-def close_pubsub(exception=None):
-    logging.debug('Closing pubsub')
-    pubsub.close()
-
-active_pubsubs = []
+# Initialize Redis connection pool and client
+pool = ConnectionPool.from_url(app.config['REDIS_URL'])
+redis_client = Redis(connection_pool=pool)
 
 def signal_handler(sig, frame):
-    logging.debug('Signal handler called')
-    for pubsub in active_pubsubs:
-        pubsub.unsubscribe('stats_channel')
+    logging.warning('Signal handler called')
     redis_client.close()
     sys.exit(0)
 
@@ -45,54 +34,40 @@ signal.signal(signal.SIGTERM, signal_handler)
 def get_visitor_count():
     return int(redis_client.get('visitor_count') or 0)
 
-def get_user_visits(ip_address):
-    return int(redis_client.hget('user_visits', ip_address) or 0)
-
 def get_unique_visitors_count():
     return redis_client.hlen('user_visits')
 
 @app.route("/")
 def index():
-    logging.debug('Index function called')
     ip_address = request.headers.get("X-Forwarded-For", request.remote_addr)
     if ip_address.startswith("10.") or ip_address.startswith("172.") or ip_address.startswith("192."):
         ip_address = "home"
     session["ip_address"] = ip_address
     redis_client.incr('visitor_count')
     redis_client.hincrby('user_visits', ip_address, 1)
-    stats = {"visitor_count": get_visitor_count(), "unique_visitors": get_unique_visitors_count(), "user_visits": get_user_visits(ip_address)}
-    logging.debug('stats updated and put into stats array')
-    stats_json = json.dumps(stats)
-    redis_client.publish('stats_channel', stats_json)
-    logging.debug('stats published to redis')
-    return render_template('index.html')
+    stats = {"visitor_count": get_visitor_count(), "unique_visitors": get_unique_visitors_count()}
+    return render_template('index.html', stats=stats)
 
 @app.route("/JonathanPolanskyResume.docx")
 def static_from_root():
-    logging.debug("JonathanPolanskyResume.docx downloaded")
     return send_from_directory(app.static_folder, request.path[1:])
 
 @app.route('/stream')
 def stream():
-    logging.debug('stream function called')
     ip_address = session.get("ip_address")
     
     def event_stream(ip_address):
-        try:
-            initial_data = json.dumps({
-                "visitor_count": get_visitor_count(),
-                "unique_visitors": get_unique_visitors_count(),
-                "user_visits": get_user_visits(ip_address)
-            })
-            yield f'data: {initial_data}\n\n'
-            for message in pubsub.listen():
-                if message['type'] == 'message':
-                    data = json.loads(message['data'].decode())
-                    if 'user_visits' in data:
-                        data['user_visits'] = get_user_visits(ip_address)
-                    yield f'data: {json.dumps(data)}\n\n'
-        except GeneratorExit:
-            logging.debug('GeneratorExit caught')
+        while True:
+            try:
+                data = {
+                    "visitor_count": get_visitor_count(),
+                    "unique_visitors": get_unique_visitors_count()
+                }
+                yield f'data: {jsonify(data)}\n\n'
+                time.sleep(1)  # delay for 1 second
+            except GeneratorExit:
+                logging.warning('GeneratorExit caught')
+                break
     
     return Response(event_stream(ip_address), mimetype='text/event-stream')
 
